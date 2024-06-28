603c37b350a6f74bca8fe568df2d62ec43882e85
MukhammadAli <133798412+MuhammadAliAsqarov@users.noreply.github.com>
2024-06-28 09:06:42 +0500

    Update permissions.py

--- management/permissions.py
+++ management/permissions.py
@@ -4,15 +4,12 @@ from rest_framework import permissions
 class IsOwnerOrSharedWith(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user is the owner of the file
-        if request.user == obj.owner:
+        if request.user == obj.first().owner:
             return True
-
         # Check if the user is in the shared_with field
-        if request.user in obj.shared_with.all():
-            # Disallow editing for shared users
-            if view.action in ['PUT', 'PATCH']:
+        if request.user in obj.first().shared_with.all():
+            if view.action in ['GET', 'PUT', 'PATCH']:
                 return False
             return True
 
-        # Default deny permission
         return False
