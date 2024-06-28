6ac5de7a222404c308f13d6673403fffd1d5de11
MukhammadAli <133798412+MuhammadAliAsqarov@users.noreply.github.com>
2024-06-28 10:11:19 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.views.static import serve
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 admin.site.site_header = 'Files Admin'
 admin.site.site_title = 'Files Admin'
