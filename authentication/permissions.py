099f62dd153a14754966e638c8d96389cdedc7f2
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-24 15:06:32 +0500

    .

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,15 +1,13 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsSuperAdminOrHR(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_authenticated and request.user.role in [3, 4]:
-            return True
-        return False
 
+def is_super_admin_or_hr(func):
+    def wrapper(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            raise CustomApiException(error_code=ErrorCodes.UNAUTHORIZED.value)
+        elif request.user.role in [3, 4]:
+            return func(self, request, *args, **kwargs)
+        raise CustomApiException(error_code=ErrorCodes.FORBIDDEN.value)
 
-class IsEmployee(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_authenticated and request.user.role in [1, 2, 3, 4]:
-            return True
-        return False
+    return wrapper
